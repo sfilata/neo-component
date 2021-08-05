c4ba1b1ecde78396be25ab5e169115153612eb9d
shu-baiqiao <shu.baiqiao@navercorp.com>
2021-08-05 13:54:41 +0800

    Feat: Add the utils file.

---
+++ src/util.js
@@ -0,0 +1,38 @@
+export function compile(str, options) {
+  templateSettings.interpolate = /{([\s\S]+?)}/g;
+  let compiled = template(str);
+  return compiled(options);
+}
+
+/**
+ *
+ * @param {T[]} arr Need to be remove duplicated elements
+ * @param {string[]} keyStr the key string arr to judge duplicated elements
+ * @returns T[] return the remove duplicated result
+ */
+export function removeDuplicate(arr, keyStr) {
+  const tempResultMap = new Map();
+  arr.forEach((item) => {
+    const key = keyStr.reduce((acc, cur) => acc + item[cur], '');
+    tempResultMap.set(key, item);
+  });
+  return [...tempResultMap.values()];
+}
+
+/**
+ *
+ * @param {the Url needed to be parsed} url
+ * @returns
+ */
+export function parseUrl(url) {
+  const searchStr = ~url.indexOf('?') ? url.slice(url.indexOf('?')) : '';
+
+  const result = {};
+  if (!searchStr.length || searchStr.length === 1) return result;
+  const paramArr = searchStr.slice(1).split('&');
+  paramArr.forEach((paramItem) => {
+    const [key, value] = paramItem.split('=');
+    result[key] = value;
+  });
+  return result;
+}
